9341c20c8059d4e7af0ab84e827fac75e1b2dc6a
Brian Timar <timarbrian@gmail.com>
2020-05-17 23:44:35 -0700

    dropped ps loop

--- ps.c
+++ ps.c
@@ -3,15 +3,13 @@
 #include "user.h"
 #include "param.h"
 
-// char* safestrcpy(char*, char*, int);
 
-void showps(struct pstat* ps, int delay) {
+void showps(struct pstat* ps) {
     int i;
 
     printf(1, "Process stats:\n-----------------\n");
     printf(1, "PID | name | state | ticket count | ticks\n---------------\n");
     
-    while (1) {
         if ((getpstat(ps)) < 0)
          {
             printf(2, "Unable to load ptstat\n");
@@ -26,17 +24,14 @@ void showps(struct pstat* ps, int delay) {
                                                         ps->ticks[i]);
             }
         }
-     sleep(delay);
-    }
    
 }
 
 int main(int argc, char* argv[]) {
 
-    int delay = 10;
     struct pstat ps;
     
-    showps(&ps, delay);
+    showps(&ps);
 
     exit();
 }
